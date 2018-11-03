0b6691e10cdcf66f5f1fb61f7e507bc916c753f9
hitler-renhao <39614457+hitler-renhao@users.noreply.github.com>
2018-11-03 16:00:55 +0800

    Revert "Revert "商家入驻页面接口待完善""
    This reverts commit 7e58077a5951f4e7f2583725cae8f5b6015bfe44.

--- js/alljs/index.js
+++ js/alljs/index.js
@@ -1,40 +1,66 @@
 $(function () {
+	// var openId = jQuery.cookie('OPENIDCOOKIENAME');
+	var userId = '';
 
-	var appointment = {
-		init: function () {
-			this.openId = jQuery.cookie('OPENIDCOOKIENAME');
-			this.userId = '';
-			this.userName = $('#name');
-			this.submit = $('#btn');
-			this.userPhone = $('#phone');
-			this.time = $('#picktime');
-			this.userCity = $('#city');
-			this.userAddress = $('#address');
-			this.remark = $('#remark');
-			this.cityChoice();
-			setTimeout(appointment.shopInfo, 2000)
+	$('#next').click(function () {
+		var shopName = $('#shopName').val(); //店名
+		var shopArea = $('#city').val(); //地区
+		var province = shopArea.split(',')[0]; // 省
+		var city = shopArea.split(',')[1]; //市
+		var area = shopArea.split(',')[2]; //区
+		var shopAddress = $('#shopAddress').val(); //详细地址
+		var shopPhone = $('#shopPhone').val(); // 电话
+		var startTime = $('#startTime').val(); // 开始营业时间
+		var endTime = $('#endTime').val(); // 结束营业时间
+		var uriImg = $('.layui-upload-list').attr('src'); //图片路径
+		var pattern = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/; // 手机号正则
+		var telephone = /\d{3}-\d{8}|\d{4}-\d{7}/;
 
-		},
-		cityChoice: function () {
-			var area2 = new LArea();
-			area2.init({
-				'trigger': '#city',
-				'valueTo': '#value',
-				'keys': {
-					id: 'value',
-					name: 'text'
-				},
-				'type': 2,
-				'data': [provs_data, citys_data, dists_data]
-			});
+		// 判断信息是否完整
+		if (!shopName || !shopArea || !shopAddress || !shopPhone || !startTime || !endTime) {
+			layer.alert('请将必填信息填写完整！');
+		} else {
+			// 验证手机号是否正确
+			if (!pattern.test(shopPhone) && !telephone.test(shopPhone)) {
+				layer.msg("请输入正确的手机号");
+				return false;
+				// 验证地址是否够
+			} else if (shopAddress.length <= 4) {
+				layer.msg('详细地址不得少于4个汉字或字母')
+			} else {
+				$.ajax({
+					type: 'GET',
+					url: global + "/shop/addShop",
+					async: true,
+					data: {
+						'addresInfo': shopArea,
+						'addres': shopAddress,
+						'shopname': shopName,
+						'phone': shopPhone,
+						'businessHours': startTime,
+						'off': 1,
+						'uriImg': uriImg,
+					},
+					success: function (data) {
+						layer.msg('上传成功，请内心等待后台审核')
+					}
+				})
+			}
 		}
-	}
-	appointment.init();
-
-
-
-
-	$('.editSuccess').click(function () {
-		location.href = 'http://wx.bjysjglasses.com/static/wxshop/html/res-chose.html'
 	})
+
+	function cityChoice() {
+		var area2 = new LArea();
+		area2.init({
+			'trigger': '#city',
+			'valueTo': '#value',
+			'keys': {
+				id: 'value',
+				name: 'text'
+			},
+			'type': 2,
+			'data': [provs_data, citys_data, dists_data]
+		});
+	}
+	cityChoice();
 })
